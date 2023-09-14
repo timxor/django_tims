1c491826aa26ffa2785ee316dc8bf57376d5238c
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-09-14 04:30:01 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,3 +142,16 @@ LOGOUT_REDIRECT_URL = "home"  # new
 # django-crispy-forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  # new
 CRISPY_TEMPLATE_PACK = "bootstrap5"  # new
+
+
+# sendgrid email config
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "tim@timsiwula.com"  # new
+
+

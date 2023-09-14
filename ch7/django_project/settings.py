064fe11323e870c4851ed45d7a4644d1aea9e59a
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-09-14 00:42:16 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- ch7/django_project/settings.py
+++ ch7/django_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # new
